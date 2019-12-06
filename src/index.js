d0cc9c2029d2f537a5134799e74a03c8b912f4eb
Artem Provornyi <artem.provornyi@gmail.com>
2019-12-06 15:29:12 +0200

    Step4: Plug in CodeClimate, Eslint, Travis-ci

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { question } from 'readline-sync';
 
-export const getUserName = () => {
+const getUserName = () => {
   const userName = question('\nMay I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
+
+export default getUserName;
